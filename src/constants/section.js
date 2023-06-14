2f8631f89e11cf040facfa44899ac61c69356c0e
shubhangi0905 <shubhangisrivastava0905@gmail.com>
2023-06-14 19:56:32 +0530

    feat: Description added

--- src/constants/section.js
+++ src/constants/section.js
@@ -1,12 +1,12 @@
 export const SECTIONS = 
 {
-    "Profile Summary": "yo whatsup",
-    "Academic and Cocurricular Achievements":"hello",
-    "Summer Internship Experience":"",
-    "Work Experience":"",
-    "Projects":"",
-    "Certifications":"",
-    "Leadership Positions": "",
-    "Extracurricular": "",
-    "Education": ""
+    "Profile Summary": "Brief summary highlighting qualifications, expertise, and career highlights.",
+    "Academic and Cocurricular Achievements":"Highlight academic and extracurricular accomplishments.",
+    "Summer Internship Experience":"Showcase relevant summer internship experience.",
+    "Work Experience":"Showcase your professional background and employment history.",
+    "Projects":"Highlight your notable achievements and practical skills",
+    "Certifications":"Showcase your professional credentials and specialized training.",
+    "Leadership Positions": "Showcase your experience and impact as a leader in various roles within organizations.",
+    "Extracurricular": " Showcase your involvement in additional activities beyond academics.",
+    "Education": "Highlight your Academic history and qualifications."
 }
